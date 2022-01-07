151b1af616e6bbd4241b03c73b3affce7126e475
angelo-gouvea <angelogouvea@linhafina.inf.br>
2022-01-07 13:30:18 -0300

    Feat:  Adicionando composer e arquivos globais

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+//date_default_timezone_set(TIME_ZONE);
+
+//new \App\Core\Core();
+
+use App\Core\Controller;
+$controller = new Controller();
+
+dd(BASE);
